f529ce00e1d203bb99451491e9ab75f03836f6cf
Lane Garner <b.lane.garner@gmail.com>
2020-10-28 11:59:58 -0500

    passing tests

--- main.js
+++ main.js
@@ -12,9 +12,28 @@ const rl = readline.createInterface({
 
 
 const pigLatin = (word) => {
+  let newWord = word.toLowerCase().trim();
+  console.log(newWord)
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let newStr = '';
+  let firstLetter = newWord[0];
+  let restOfWord = newWord.substring(1)
 
-  // Your code here
+  if (vowels.indexOf(newWord[0]) > -1) {
+      newStr = newWord + "yay";
+      return newStr;
+  } else {
+    let firstMatch = newWord.match(/[aeiou]/g) || 0;
+    let vowel = newWord.indexOf(firstMatch[0]);
+    newStr = newWord.substring(vowel) + newWord.substring(0, vowel) + "ay";
+    return newStr;
+  }
+  
+  // else {
 
+  // return(restOfWord + firstLetter + 'ay')
+  // return(translateWord)
+  // }
 }
 
 // the first function called in the program to get an input from the user
