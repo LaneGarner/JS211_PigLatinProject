35fa7b4afa35290de624dc2aad4f7d175ae0c301
Lane Garner <b.lane.garner@gmail.com>
2020-11-04 19:37:57 -0600

    gui working

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const pigLatin = (word) => {
+const output = (str) => document.querySelector('#display-element').innerHTML = str;
+// let img = document.createElement("img"); 
+// img.src = "cutePig.jpg"; 
+// let src = document.querySelector('#display-element'); 
+ 
+// src.appendChild(img); 
+
+
+
+const pigLatin = () => {
+    word = document.querySelector('#word-input').value;
     let newWord = word.toLowerCase().trim();
-    console.log(newWord)
+    let words = newWord.split(" ")
+    words.toString
+    console.log(words)
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let newStr = '';
-  
-    if (vowels.indexOf(newWord[0]) > -1) {
-        newStr = newWord + "yay";
-        return newStr;
+
+    if (word === '') {
+      alert('Please enter a word')
+      output('')
+      // document.querySelector('#display-element').innerHTML = str;
+    }
+    else if (vowels.indexOf(newWord[0]) > -1) {
+      newStr = newWord + "yay";
+      output(newStr);
     } else {
       let firstMatch = newWord.match(/[aeiou]/g) || 0;
       let vowel = newWord.indexOf(firstMatch[0]);
       newStr = newWord.substring(vowel) + newWord.substring(0, vowel) + "ay";
-      return newStr;
+      output(newStr);
     }
   }
-
-console.log(pigLatin('cat'))
+  
